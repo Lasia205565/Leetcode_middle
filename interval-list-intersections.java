185bf9c2dbdf42cd18d7449e2eb331b0c4a2626f
Lasia205565 <3164432887@qq.com>
2019-02-03 14:02:42 +0800

    Update interval-list-intersections.java

--- interval-list-intersections.java
+++ interval-list-intersections.java
@@ -5,6 +5,17 @@
 （形式上，闭区间 [a, b]（其中 a <= b）表示实数 x 的集合，而 a <= x <= b。两个闭区间的交集是一组实数，要么为空集，要么为闭区间。例如，[1, 3] 和 [2, 4] 的交集为 [2, 3]。）
 
 */
+
+/***Summary**
+1.在java中怎么把list转换为数组：
+     eg. List<Interval> result = new ArrayList<> ();
+          Interval answer [] = result.toArray(new Interval[result.size()])
+
+*/
+
+
+
+
 /**
  * Definition for an interval.
  * public class Interval {
